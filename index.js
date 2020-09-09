97456d72df69c32411ffd6c08a1e632800f6fbdd
linhle <lklinh@outlook.com>
2020-09-09 09:52:16 +0700

    final

--- index.js
+++ index.js
@@ -3,17 +3,6 @@ let taskList = [];
 // Get the input field
 var input = document.getElementById("task");
 
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function (event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("addTask").click();
-  }
-});
-
 const addTodo = () => {
   let input = document.getElementById("task").value;
   if (input == "" || input == " ") {
