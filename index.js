bfeb904b04226a7c3c72a96f9d2516c34c31093b
linhle <lklinh@outlook.com>
2020-09-09 09:45:55 +0700

    final

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ input.addEventListener("keyup", function (event) {
 
 const addTodo = () => {
   let input = document.getElementById("task").value;
-  taskList.push({ taskName: input, isDone: false });
+  if (input == "" || input == " ") {
+    alert("invalid task");
+  } else {
+    taskList.push({ taskName: input, isDone: false });
+  }
   console.log(taskList);
   render();
 };
@@ -25,28 +29,66 @@ let render = () => {
   let result = taskList
     .map(
       (item, index) =>
-        `
-        <li class="item" id="${index}">
-        <input type="checkbox" onclick="check(${index})"/>
-        <span>${item.taskName}</span>
-        <button onclick="remove(${index})">x</button>
-        </li>`
+        `<div class="item">
+        <input id="checkBox" type="checkbox" onclick="check(${index})"/>
+        <span id="${index}">${item.taskName}</span>
+        <button id="removeBttn" onclick="remove(${index})">x</button>
+        </div>`
     )
     .join("");
   document.getElementById("taskArea").innerHTML = result;
+  return;
 };
 
 let remove = (index) => {
   taskList.splice(index, 1);
   render();
-  console.log(taskList);
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].isDone) {
+      document.getElementById("checkBox").checked = true;
+      document.getElementById(i).style.textDecoration = "line-through";
+      return;
+    } else {
+      document.getElementById("checkBox").checked = false;
+      document.getElementById(i).style.textDecoration = "none";
+      return;
+    }
+  }
 };
 
 let check = (index) => {
   taskList[index].isDone = !taskList[index].isDone;
   if (taskList[index].isDone) {
     document.getElementById(index).style.textDecoration = "line-through";
+    return;
   } else {
     document.getElementById(index).style.textDecoration = "none";
+    return;
   }
 };
+
+// month
+var d = new Date();
+var months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+document.getElementById("month").innerHTML = months[
+  d.getMonth()
+].toLocaleUpperCase();
+// date
+var d = new Date();
+document.getElementById("date").innerHTML = d.getDate();
+// year
+var d = new Date();
+document.getElementById("year").innerHTML = d.getFullYear();
